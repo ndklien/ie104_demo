f2c0c9ba74d3658ac1592f6a0eb4f4d7c3d13dd8
Lien Nguyen <ndklien3004@gmail.com>
2020-11-17 14:54:09 +0700

    Edit views in polls

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from .forms import PushQuestionForm
 from django.urls import reverse
 from django.views import generic
