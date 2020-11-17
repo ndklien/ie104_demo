f38f583214468e4564f207ccc76d4e9403f0cfd6
Lien Nguyen <ndklien3004@gmail.com>
2020-11-17 10:19:23 +0700

    add elasticsearch app

---
+++ mysite/polls/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Question, Choice
+
+# Register your models here.
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin:
+    fieldset = [
+        (None, {'fields': ['question_text']}), 
+        ('Date information', {'fields': ['pub_date']})
+    ]
+    inlines = [ChoiceInLine]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+
+
+admin.site.register(Question)
+admin.site.register(Choice)
